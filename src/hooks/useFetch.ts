d7ad3fec8c0a301d6181a575482d3367349e8a40
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-02 17:13:57 -0300

    fix: :zap: API Url changed

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://restfulapi-ax4b.herokuapp.com/",
 });
 
 // GET
--- src/hooks/useInsert.ts
+++ src/hooks/useInsert.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://restfulapi-ax4b.herokuapp.com/",
 });
 
 export async function insertData<T = unknown>(
